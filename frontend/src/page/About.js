e07d60de48420e123f07caef1180918561ad8738
Rehan alam <alamrehan12345678@gmail.com>
2024-08-03 17:58:22 +0530

    about

--- frontend/src/page/About.js
+++ frontend/src/page/About.js
@@ -6,31 +6,31 @@ import { Menu, X, MapPin } from 'lucide-react'
 
 
 const locations = [
+  // {
+  //   title: 'Bengaluru office',
+  //   timings: 'Mon-Sat 9am to 5pm.',
+  //   address: '100, Electronic City Phase-1, Bengaluru, Karnataka 560100 IN',
+  // },
   {
-    title: 'Bengaluru office',
+    title: 'abhay madarchod ',
     timings: 'Mon-Sat 9am to 5pm.',
-    address: '100, Electronic City Phase-1, Bengaluru, Karnataka 560100 IN',
+    address: 'Harnihaya -4 Bara , Nepal',
   },
   {
-    title: 'Head office',
+    title: 'kaliya  office',
     timings: 'Mon-Sat 9am to 5pm.',
-    address: '12th Main Rd, Indiranagar, Bengaluru, Karnataka 560008 IN',
-  },
-  {
-    title: 'Karnataka office',
-    timings: 'Mon-Sat 9am to 5pm.',
-    address: '42, Residency Rd, Shanthala Nagar, Ashok Nagar, Bengaluru, Karnataka 560025 IN',
+    address: 'kaliya-15 bara',
   },
 ]
 
 const users = [
-  {
-    name: 'Gabrielle Fernandez',
-    image:
-      'https://images.unsplash.com/photo-1531746020798-e6953c6e8e04?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=600&w=600&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
-    position: 'Marketing Lead',
-  },
   // {
+  //   name: 'Gabrielle Fernandez',
+  //   image:
+  //     'https://images.unsplash.com/photo-1531746020798-e6953c6e8e04?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=600&w=600&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
+  //   position: 'Marketing Lead',
+  // },
+  // // {
   //   name: 'Victória Silva',
   //   image:
   //     'https://images.generated.photos/vBRCiI_3UM4l40sU8s7fCwbJwzDwRTGpebzPkfHFsY4/rs:fit:512:512/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/ODgyMTAyLmpwZw.jpg',
@@ -60,18 +60,18 @@ const users = [
   //     'https://images.generated.photos/lEhPUYYfYr9IOt13WZZGf2zbXZnCYdU1wUp7gcu_99c/rs:fit:512:512/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92M18w/Njg5NTQ5LmpwZw.jpg',
   //   position: 'Front-end developer',
   // },
-  {
-    name: 'Jordi Santiago',
-    image:
-      'https://images.pexels.com/photos/2269872/pexels-photo-2269872.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    position: 'Front-end developer',
-  },
-  {
-    name: 'Kerim Fahri',
-    image:
-      'https://images.unsplash.com/photo-1456327102063-fb5054efe647?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=600&w=600',
-    position: 'Back-end developer',
-  },
+  // {
+  //   name: 'Jordi Santiago',
+  //   image:
+  //     'https://images.pexels.com/photos/2269872/pexels-photo-2269872.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+  //   position: 'Front-end developer',
+  // },
+  // {
+  //   name: 'Kerim Fahri',
+  //   image:
+  //     'https://images.unsplash.com/photo-1456327102063-fb5054efe647?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=600&w=600',
+  //   position: 'Back-end developer',
+  // },
 ]
 
 export function About() {
@@ -102,7 +102,7 @@ export function About() {
             </span>
             <span className="font-bold"></span>
           </div>
-          
+
           <div className="hidden lg:block">
             <button
               type="button"
@@ -147,12 +147,12 @@ export function About() {
                       </button>
                     </div>
                   </div>
-                 
+
                   <button
                     type="button"
                     className="mt-4 w-full rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
                   >
-                   
+
                   </button>
                 </div>
               </div>
@@ -168,11 +168,10 @@ export function About() {
             <p className="text-xs font-semibold leading-normal md:text-sm">About the company</p>
           </div>
           <p className="text-3xl font-bold text-gray-900 md:text-5xl md:leading-10">
-            Made with love, right here in India
+            Made with love, right here in Nepal
           </p>
           <p className="max-w-4xl text-base text-gray-600 md:text-xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore veritatis voluptates
-            neque itaque repudiandae sint, explicabo assumenda quam ratione placeat?
+            Welcome to NH Kirana Store, your one-stop shop for all your grocery needs! Located in the heart of the neighborhood, we pride ourselves on offering a wide range of high-quality products at affordable prices. From fresh fruits and vegetables to pantry staples and household essentials, we've got you covered. Our friendly and knowledgeable staff are always ready to assist you in finding exactly what you need. With convenient hours and a welcoming atmosphere, NH Kirana Store is the perfect place to stock up on groceries while supporting local business. Experience the convenience and warmth of shopping with us today!
           </p>
         </div>
         <div className="w-full space-y-4">
@@ -255,24 +254,19 @@ export function About() {
         <footer className="px-4 py-10">
           <div className="flex flex-col md:flex-row md:items-center">
             <span>
-              <svg
+              {/* <svg
                 width="40"
                 height="46"
                 viewBox="0 0 50 56"
                 fill="none"
-                xmlns="http://www.w3.org/2000/svg"
+                // xmlns="http://www.w3.org/2000/svg"
               >
                 <path
-                  d="M23.2732 0.2528C20.8078 1.18964 2.12023 12.2346 1.08477 13.3686C0 14.552 0 14.7493 0 27.7665C0 39.6496 0.0986153 41.1289 0.83823 42.0164C2.12023 43.5449 23.2239 55.4774 24.6538 55.5267C25.9358 55.576 46.1027 44.3832 48.2229 42.4602C49.3077 41.474 49.3077 41.3261 49.3077 27.8158C49.3077 14.3055 49.3077 14.1576 48.2229 13.1714C46.6451 11.7415 27.1192 0.450027 25.64 0.104874C24.9497 -0.0923538 23.9142 0.00625992 23.2732 0.2528ZM20.2161 21.8989C20.2161 22.4906 18.9835 23.8219 17.0111 25.3997C15.2361 26.7803 13.8061 27.9637 13.8061 28.0623C13.8061 28.1116 15.2361 29.0978 16.9618 30.2319C18.6876 31.3659 20.2655 32.6479 20.4134 33.0917C20.8078 34.0286 19.871 35.2119 18.8355 35.2119C17.8001 35.2119 9.0233 29.3936 8.67815 28.5061C8.333 27.6186 9.36846 26.5338 14.3485 22.885C17.6521 20.4196 18.4904 20.0252 19.2793 20.4196C19.7724 20.7155 20.2161 21.3565 20.2161 21.8989ZM25.6893 27.6679C23.4211 34.9161 23.0267 35.7543 22.1391 34.8668C21.7447 34.4723 22.1391 32.6479 23.6677 27.9637C26.2317 20.321 26.5275 19.6307 27.2671 20.3703C27.6123 20.7155 27.1685 22.7864 25.6893 27.6679ZM36.0932 23.2302C40.6788 26.2379 41.3198 27.0269 40.3337 28.1609C39.1503 29.5909 31.6555 35.2119 30.9159 35.2119C29.9298 35.2119 28.9436 33.8806 29.2394 33.0424C29.3874 32.6479 30.9652 31.218 32.7403 29.8867L35.9946 27.4706L32.5431 25.1532C30.6201 23.9205 29.0915 22.7371 29.0915 22.5892C29.0915 21.7509 30.2256 20.4196 30.9159 20.4196C31.3597 20.4196 33.6771 21.7016 36.0932 23.2302Z"
-                  fill="black"
-                />
-              </svg>
+                //   d="M23.2732 0.2528C20.8078 1.18964 2.12023 12.2346 1.08477 13.3686C0 14.552 0 14.7493 0 27.7665C0 39.6496 0.0986153 41.1289 0.83823 42.0164C2.12023 43.5449 23.2239 55.4774 24.6538 55.5267C25.9358 55.576 46.1027 44.3832 48.2229 42.4602C49.3077 41.474 49.3077 41.3261 49.3077 27.8158C49.3077 14.3055 49.3077 14.1576 48.2229 13.1714C46.6451 11.7415 27.1192 0.450027 25.64 0.104874C24.9497 -0.0923538 23.9142 0.00625992 23.2732 0.2528ZM20.2161 21.8989C20.2161 22.4906 18.9835 23.8219 17.0111 25.3997C15.2361 26.7803 13.8061 27.9637 13.8061 28.0623C13.8061 28.1116 15.2361 29.0978 16.9618 30.2319C18.6876 31.3659 20.2655 32.6479 20.4134 33.0917C20.8078 34.0286 19.871 35.2119 18.8355 35.2119C17.8001 35.2119 9.0233 29.3936 8.67815 28.5061C8.333 27.6186 9.36846 26.5338 14.3485 22.885C17.6521 20.4196 18.4904 20.0252 19.2793 20.4196C19.7724 20.7155 20.2161 21.3565 20.2161 21.8989ZM25.6893 27.6679C23.4211 34.9161 23.0267 35.7543 22.1391 34.8668C21.7447 34.4723 22.1391 32.6479 23.6677 27.9637C26.2317 20.321 26.5275 19.6307 27.2671 20.3703C27.6123 20.7155 27.1685 22.7864 25.6893 27.6679ZM36.0932 23.2302C40.6788 26.2379 41.3198 27.0269 40.3337 28.1609C39.1503 29.5909 31.6555 35.2119 30.9159 35.2119C29.9298 35.2119 28.9436 33.8806 29.2394 33.0424C29.3874 32.6479 30.9652 31.218 32.7403 29.8867L35.9946 27.4706L32.5431 25.1532C30.6201 23.9205 29.0915 22.7371 29.0915 22.5892C29.0915 21.7509 30.2256 20.4196 30.9159 20.4196C31.3597 20.4196 33.6771 21.7016 36.0932 23.2302Z"
+                //   fill="black"
+                // />
+              </svg> */}
             </span>
-            <div className="mt-4 grow md:ml-12 md:mt-0">
-              <p className="text-base font-semibold text-gray-700">
-                © 2023 DevUI Component Library
-              </p>
-            </div>
           </div>
           <div className="mt-16 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
             <div className="mb-8 lg:mb-0">
@@ -292,7 +286,7 @@ export function About() {
                 <li>New Hampshire</li>
                 <li>Maine</li>
                 <li>Texas</li>
-                <li>Indiana</li>
+                <li>Nepal</li>
               </ul>
             </div>
             <div className="mb-8 lg:mb-0">
